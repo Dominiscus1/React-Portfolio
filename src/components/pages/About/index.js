39d2365ff0f55961f46e58b89bcfc99588c478f1
Anthony Jimenez <tony6776a@gmail.com>
2022-01-20 15:10:36 -0500

    edited file structure

--- src/components/pages/About/index.js
+++ src/components/pages/About/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import "./style.css";
 
 export default function About() {
   return (
-    <div>
+    <div className="aboutName">
       <h1>About Page</h1>
       <p>
         Nunc pharetra finibus est at efficitur. Praesent sed congue diam.
